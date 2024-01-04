85d1359e4a9d052d0a2ba9505c182512bdecf4bc
RafatBSaqqa <raafat.saqqa@gmail.com>
2024-01-05 02:12:22 +0300

    Part 2 complete all course endpoint

--- backend/controllers/course.js
+++ backend/controllers/course.js
@@ -62,43 +62,83 @@ const getAllCourseByCategoryId = async (req, res) => {
 // this function update course by id 
 const updateCourseById = (req, res) => {
     const { courseId } = req.params;
-    // const decoded = jwtDecode(req.headers.authorization)
-    // console.log(decoded.id);
-        courseSchema.findByIdAndUpdate({ _id: courseId}, req.body, { new: true })
-        .then((result) => {
-            res.status(200).json({
-                success:true,
-                message:"Course updated successfuly",
-                done:result
-            })
+    const decoded = jwtDecode(req.headers.authorization)
+    courseSchema.findOne({ _id: courseId })
+        .then((owner) => {
+            if (decoded.id == owner.owner) {
+                courseSchema.findByIdAndUpdate({ _id: courseId }, req.body, { new: true })
+                    .then((result) => {
+                        res.status(200).json({
+                            success: true,
+                            message: "Course updated successfuly",
+                            done: result
+                        })
+                    })
+                    .catch((err) => {
+                        res.status(404).json({
+                            success: false,
+                            message: "Server error",
+                            error: err
+                        })
+                    })
+            } else {
+                res.status(403).json({
+                    success:false,
+                    message: "unaothorized to this course"
+                })
+            }
         })
         .catch((err) => {
             res.status(404).json({
                 success:false,
-                message: "Server error",
+                message: "server error",
                 error:err
             })
         })
+
 }
 
 // This function delete course by id
-const deleteCourseById = async (req,res) => {
-    const courseId = req.params.courseId
-    try {
-        const deleted = await courseSchema.findOneAndDelete({_id:courseId})
-        res.status(200).json({
-            success:true,
-            message : "Course deleted successfuly",
-            done:deleted
-        })
-    } catch (error) {
-        res.status(404).json({
-            success:false,
-            message : "server error",
-            err:error
-        })
-    }
+const deleteCourseById = async (req, res) => {
+    const {courseId} = req.params
+    const decoded = jwtDecode(req.headers.authorization)
+    courseSchema.findOne({ _id: courseId }).then((owner) => {
+        if (decoded.id == owner.owner) {
+            courseSchema.findOneAndDelete({ _id: courseId })
+            .then((deleted) => {
+               if (deleted) {
+                   res.status(200).json({
+                       success: true,
+                       message: "Course deleted successfuly",
+                       done: deleted
+                   })
+               } else {
+                   res.status(404).json({
+                       success: false,
+                       message: "Not found",
+                   })
+               }
+            })
+            .catch((err) => {
+               res.status(404).json({
+                   success: false,
+                   message: "server error",
+                   error: err
+               })
+            })
+        } else {
+            res.status(403).json({
+                success:false,
+                message : "unauthorized to this course",
+            })
+        }
+    })
+
+        
+        
+
    
+
 }
 
 module.exports = {
