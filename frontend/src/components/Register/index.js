2f482a0a71106d9feef0dd4b88751b5888607f1a
RafatBSaqqa <raafat.saqqa@gmail.com>
2024-01-09 17:54:50 +0300

    Add style to the register page

--- frontend/src/components/Register/index.js
+++ frontend/src/components/Register/index.js
@@ -1,73 +1,146 @@
-import './style.css';
-import { useState ,useContext} from 'react';
-import axios from 'axios'
-import { UserContext } from '../../App';
+import "./style.css";
+import { useState, useContext } from "react";
+import axios from "axios";
+import { UserContext } from "../../App";
 const Register = () => {
-    const user = useContext(UserContext);
+  const user = useContext(UserContext);
 
-    const [name, setName] = useState();
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
-    const [age, setAge] = useState();
-    const [phoneNumber, setPhoneNumber] = useState();
-    const [country, setCountry] = useState();
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [age, setAge] = useState();
+  const [phoneNumber, setPhoneNumber] = useState();
+  const [country, setCountry] = useState();
 
-    return (
-        <div className="register">
-            <h1>Rigester component</h1>
-            <div className='form'>
-                <label>Name</label>
-                <input placeholder='Name' onChange={(e) => {
-                    setName(e.target.value)
-                }} />
-                <br />
-                <label>Email</label>
-                <input placeholder='Email' onChange={(e) => {
-                    setEmail(e.target.value)
-                }} />
-                <br />
-                <label>Password</label>
-                <input placeholder='Password' onChange={(e) => {
-                    setPassword(e.target.value)
-                }} />
-                <br />
-                <label>Age</label>
-                <input placeholder='Age' onChange={(e) => {
-                    setAge(e.target.value)
-                }} />
-                <br />
-                <label>Phone number</label>
-                <input placeholder='PhoneNumber' onChange={(e) => {
-                    setPhoneNumber(e.target.value)
+  return (
+    <div className="inputform-reg">
+        
+       <div className="desc-login">
+        <h1>Learn From The Expert</h1>
+        <p className="parg-login">Lore LoreLoreLorepLoreLoreLoreLoreLoreLoreLoreLo<br />reLoreLoreLore</p>
+        <button className="subsc-btn btn">Subsecrib Now</button>
+      </div>
 
-                }} />
-                <br />
-                <label>Country</label>
-                <input placeholder='Country' onChange={(e) => {
-                    setCountry(e.target.value)
-                }} />
-                <br />
-                <button onClick={() => {
-                    const userInfo = {
-                        name,
-                        email,
-                        password,
-                        age,
-                        country,
-                        phoneNumber,
-                        role: "6595c8431aa2ed8204c495c2"
-                    }
-                    axios.post('http://localhost:5000/user/register', userInfo).then((res) => {
-                        user.Navigate('/login')
-                        console.log(res);
-                    })
-                        .catch((err) => {
-                            console.log(err);
-                        })
-                }}>Register</button>
-            </div>
+      <div className="register-component">
+      <h1>Sign Up</h1>
+        <div className="input-container">
+          <input
+            className="input-fieldreg"
+            placeholder=" "
+            type="text"
+            id="name"
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+          <label for="name" className="input-label">
+            Name
+          </label>
         </div>
-    );
-}
 
-export default Register
+        <div className="input-container">
+          <input
+            type="email"
+            id="email"
+            className="input-fieldreg"
+            placeholder=" "
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <label for="email" className="input-label">
+            Email
+          </label>
+        </div>
+
+        <div className="input-container">
+          <input
+            placeholder=" "
+            className="input-fieldreg"
+            type="password"
+            id="password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <label for="password" className="input-label">
+            Password
+          </label>
+        </div>
+
+        <div className="input-container">
+          <input
+            type="number"
+            id="age"
+            className="input-fieldreg"
+            placeholder=" "
+            onChange={(e) => {
+              setAge(e.target.value);
+            }}
+          />
+          <label for="age" className="input-label">
+            Age
+          </label>
+        </div>
+
+        <div className="input-container">
+          <input
+            type="number"
+            id="phoneNumber"
+            className="input-fieldreg"
+            placeholder=" "
+            onChange={(e) => {
+              setPhoneNumber(e.target.value);
+            }}
+          />
+          <label for="phoneNumber" className="input-label">
+            Phone number
+          </label>
+        </div>
+
+        <div className="input-container">
+          <input
+            type="text"
+            id="country"
+            className="input-fieldreg"
+            placeholder=" "
+            onChange={(e) => {
+              setCountry(e.target.value);
+            }}
+          />
+          <label for="country" className="input-label">
+            Country
+          </label>
+        </div>
+
+        <button className="register-btn btn"
+          onClick={() => {
+            const userInfo = {
+              name,
+              email,
+              password,
+              age,
+              country,
+              phoneNumber,
+              role: "6595c8431aa2ed8204c495c2",
+            };
+            axios
+              .post("http://localhost:5000/user/register", userInfo)
+              .then((res) => {
+                user.Navigate("/login");
+                console.log(res);
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          }}
+        >
+          Register
+        </button>
+
+      </div>
+    </div>
+  );
+};
+
+export default Register;
