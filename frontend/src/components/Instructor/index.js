90d3872dbe59de7a7e16068791cb7bb7446b10a4
RafatBSaqqa <raafat.saqqa@gmail.com>
2024-01-15 14:12:20 +0300

    add instructor actions

--- frontend/src/components/Instructor/index.js
+++ frontend/src/components/Instructor/index.js
@@ -1,10 +1,63 @@
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
+import { useContext } from "react";
+import { UserContext } from "../../App";
+
+
 const Instructor = () => {
+
+  const {Navigate} = useContext(UserContext)
+
   return (
     <div>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
+      <Container>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Action name</TableCell>
+                <TableCell align="right">Actions</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  Course
+                </TableCell>
+                <TableCell align="right">
+                  <Button onClick={()=> {
+                    Navigate('/courseActions')
+                  }} variant="outlined" color="success">
+                    Action
+                  </Button>
+                </TableCell>
+              </TableRow>
+              <TableRow
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  Upload Video
+                </TableCell>
+                <TableCell align="right">
+                  {" "}
+                  <Button variant="outlined" color="success">
+                    Action
+                  </Button>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Container>
     </div>
   );
 };
